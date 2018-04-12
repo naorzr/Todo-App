4833cf97edbdec442f4ad3b137f65528473b95d3
Naor <naorzruk@gmail.com>
2018-04-12 21:35:38 +0300

    test cases for 404

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -11,18 +11,18 @@ if (!ObjectID.isValid(id) || !ObjectID.isValid(userId)){
   return console.log('ID not valid');
 }
 
-// Todo.find({
-//   _id: id
-// }).then((todos) => {
-//   console.log('Todos',todos);
-// })
-//
-//
-// Todo.findOne({
-//   _id: id
-// }).then((todo) => {
-//   console.log('Todo',todo);
-// })
+Todo.find({
+  _id: id
+}).then((todos) => {
+  console.log('Todos',todos);
+})
+
+
+Todo.findOne({
+  _id: id
+}).then((todo) => {
+  console.log('Todo',todo);
+})
 
 Todo.findById(id).then((todo) => {
   if(!todo){
