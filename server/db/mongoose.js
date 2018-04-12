203db8746a8fd898f72c3cc309b528c81e93f45c
Naor <naorzruk@gmail.com>
2018-04-12 22:57:58 +0300

    a

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var mongodb_uri = 'mongodb://<dbuser>:<dbpassword>@ds229008.mlab.com:29008/mongoose-for-nodejs-mongodb-puralsight';
+
+mongoose.connect(mongodb_uri || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
