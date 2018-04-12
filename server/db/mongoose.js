53fba50b170db201752ecccdce222f44193e7748
Naor <naorzruk@gmail.com>
2018-04-12 23:41:09 +0300

    setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-var mongodb_uri = 'mongodb://<dbuser>:<dbpassword>@ds229008.mlab.com:29008/mongoose-for-nodejs-mongodb-puralsight';
+var mongodb_uri = 'mongodb://<naorzr>:<34543454>@ds229008.mlab.com:29008/mongoose-for-nodejs-mongodb-puralsight';
 
 mongoose.connect(mongodb_uri || 'mongodb://localhost:27017/TodoApp');
 
