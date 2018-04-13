36d4f3c86c18c7681b6fb9df9348381f72e8cc5e
Naor <naorzruk@gmail.com>
2018-04-13 12:55:02 +0300

    ..

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,8 @@ var {PORT} = require('./../server.js');
 
 mongoose.Promise = global.Promise;
 var mongodb_uri = 'mongodb://naorzr:34543454@ds229008.mlab.com:29008/mongoose-for-nodejs-mongodb-puralsight';
-var dbConnection = PORT!==3000 ?mongodb_uri:'mongodb://localhost:27017/TodoApp';
+var localDb = 'mongodb://localhost:27017/TodoApp'
+var dbConnection = process.env.mongodb_uri || localDb;
 mongoose.connect(dbConnection);
 console.log(`Connected to ${dbConnection}`)
 module.exports = {mongoose};
