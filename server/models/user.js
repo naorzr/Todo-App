10ee1d40b600d7a75ee5144b062066195c0605ea
Naor <naorzruk@gmail.com>
2018-04-17 22:50:45 +0300

    Added a middleware for saving a new user

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const validator = require('validator');
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 
 var UserSchema = new mongoose.Schema({
@@ -69,6 +70,23 @@ UserSchema.statics.findByToken = function (token) {
     'tokens.access': 'auth'
   });
 };
+
+UserSchema.pre('save',function (next) {
+  var user = this;
+
+  if (user.isModified('password')) {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        console.log(err);
+        console.log(user,user.password);
+        next();
+      })
+    });
+  } else {
+    next();
+  }
+});
 
 
 var User = mongoose.model('User', UserSchema);
