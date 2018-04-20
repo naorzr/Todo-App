2c9760961af06f119da4b2878c2d983143319259
Naor <naorzruk@gmail.com>
2018-04-20 15:13:24 +0300

    Added a delete user route

--- server/server.js
+++ server/server.js
@@ -130,6 +130,14 @@ app.get('/users/me', authenticate ,(req, res) => {
   res.send(req.user);
 });
 
+app.delete('/users/me/token', authenticate ,(req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.send();
+  }, () => {
+    res.status(400).send();
+  })
+});
+
 app.listen(PORT, () => {
   console.log(`Started on port ${PORT}`);
 });
