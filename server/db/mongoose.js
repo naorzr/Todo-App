88c18a5b57304cb003a184f85776a131081cef1a
Naor <naorzruk@gmail.com>
2018-04-23 19:36:40 +0300

    a

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,9 +4,8 @@ var {PORT} = require('./../server.js');
 mongoose.Promise = global.Promise;
 
 
-mongoose.connect(process.env.MONGODB_URI,  auth: {
-      user: '<naor>',
-      password: '<34543454>'
-    });
+mongoose.connect(process.env.MONGODB_URI,  {useMongoClient: true}, (err) => {
+  console.log("Error in connecting to mongodb via mongoose.connect",err);
+});
 console.log(`Connected to ${process.env.MONGODB_URI}`)
 module.exports = {mongoose};
